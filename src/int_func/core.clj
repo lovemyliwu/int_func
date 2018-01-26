00daaa9422122412d5faa1f1b12c0f0f59c867b9
Smite Chow <xiaopengyou2no1@gmail.com>
2018-01-27 00:42:15 +0800

    improve int logic

--- src/int_func/core.clj
+++ src/int_func/core.clj
@@ -1,12 +1,31 @@
 (ns int-func.core
-  (:use int-func.utils)
-  (:require
-    [int-func.utils :as utils]))
+  (:use int-func.utils))
+
+#_("
+Python Example
+
+number = None
+for item in string:
+    if number is None:
+        number = int(x)
+    else:
+      number = 10 * number + int(x)
+print(number)
+")
+
+(defn main
+  "main function"
+  [targetString]
+  (println (format "parse %s to int" targetString))
+  (reduce #(+ (* %1 10) %2)
+          (for [item targetString
+                :when (re-matches #"\d" (str item))
+                :let [intItem (parseInt (str item))]
+                ]
+            intItem))
+  )
 
 (defn -main
   "the entry point"
-  [name number-string]
-  (say
-    (format "smite %s" name))
-  (print
-    (utils/myint number-string)))
+  [arg & args]
+  (println (main arg)))
--- src/int_func/utils.clj
+++ src/int_func/utils.clj
@@ -1,9 +1,21 @@
 (ns int-func.utils)
 
-(defn myint [number-string]
-  (Integer. number-string))
+(def mapping
+  {
+   :0 0,
+   :1 1,
+   :2 2,
+   :3 3,
+   :4 4,
+   :5 5,
+   :6 6,
+   :7 7,
+   :8 8,
+   :9 9,
+   })
 
-(defn say
-  "I don't do a whole lot."
-  [name]
-  (println name "Hello, World!"))
+(defn parseInt
+  "parse char to int"
+  [item]
+  (println (format "parse %s to int" item))
+  ((keyword (str item)) mapping))
